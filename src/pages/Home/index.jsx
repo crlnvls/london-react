a58f98a70101abdab9cb9852642cc521c70e7444
“crlnvls” <cveloso@hotmail.co.uk>
2023-04-16 21:37:57 +0100

    Add components to Header and fix background img

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./Home.css";
 import Header from "../../components/Header";
+import Explore from "../../components/Explore";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Explore />
     </>
   );
 };
