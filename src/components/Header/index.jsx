6bf82c755019dcc6ca570f9d225db4c3a4efffef
“crlnvls” <cveloso@hotmail.co.uk>
2023-04-16 21:42:17 +0100

    Add heading to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import "./Header.css";
 
 import NavBar from "../NavBar";
+import Heading from "../Heading";
 
 const Header = () => {
   return (
     <>
       <div className="header">
         <NavBar />
+        <Heading />
       </div>
     </>
   );
