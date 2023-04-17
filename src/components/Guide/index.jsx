54491a721402b8d7b8a1221ed7b97a94efbae5e8
“crlnvls” <cveloso@hotmail.co.uk>
2023-04-17 23:01:55 +0100

    Add guide component

--- src/components/Guide/index.jsx
+++ src/components/Guide/index.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 import "./Guide.css";
+import Button from "../Button";
 
 const Guide = () => {
   return (
     <>
-      <div className="guide">
+      <div className="guide-div" id="guide">
         <div className="guide-flex">
-          <h3 className="guide-heading">
-            Choose a knowledgeable and experienced local guide
-          </h3>
           <img className="guide-img" src="/guide.png" alt="Tour Guide" />
+          <div className="guide-heading">
+            <h3 className="guide-h">
+              Choose a <span className="cursive">knowledgeable</span> and{" "}
+              <span className="cursive">experienced </span>guide
+            </h3>
+            <p className="guide-p">
+              A reliable guide can enhance your London tourism experience by
+              providing insightful information and personalized recommendations.
+            </p>
+            <Button text={"Book a guide"} />
+          </div>
         </div>
       </div>
     </>
