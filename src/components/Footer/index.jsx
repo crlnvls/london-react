c2322ee0e540f846d6353c20f61b2867f502adf2
“crlnvls” <cveloso@hotmail.co.uk>
2023-04-21 15:39:36 +0100

    Create and style footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,12 +6,23 @@ const Footer = () => {
   return (
     <>
       <div className="footer-div">
-        <img
-          className="footer-img"
-          src="/underground.png"
-          alt="underground icon"
-        />
-        <h3 className="footer-h">Contact Us</h3>
+        <div>
+          <h3 className="footer-h">Contact Us</h3>
+          <div>
+            <img src="/email.png" alt="email icon" className="socials" />
+            <img
+              src="/instagram.png"
+              alt="instagram icon"
+              className="socials"
+            />
+            <img src="/facebook.png" alt="facebook icon" className="socials" />
+            <img src="/twitter.png" alt="twitter icon" className="socials" />
+            <p className="info-footer">Info · Support · Marketing</p>
+            <p className="info-footer">Terms of Use · Privacy Policy</p>
+            <p className="info-footer">© 2023 London</p>
+          </div>
+        </div>
+        <img src="/underground.png" alt="underground icon" />
       </div>
     </>
   );
